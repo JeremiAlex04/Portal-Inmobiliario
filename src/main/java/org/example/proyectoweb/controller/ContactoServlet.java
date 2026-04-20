34eb7c3bf2d332b92bddf3faa5cc05a464a59bc6
miranda2608 <mirandadharma08@gmail.com>
2026-04-20 00:18:44 -0500

    Update ContactoServlet.java

--- src/main/java/org/example/proyectoweb/controller/ContactoServlet.java
+++ src/main/java/org/example/proyectoweb/controller/ContactoServlet.java
@@ -1,1 +1,41 @@
+package org.example.proyectoweb.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import org.example.proyectoweb.dao.ContactoDAO;
+import org.example.proyectoweb.model.Contacto;
+
+import java.io.IOException;
+
+@WebServlet("/contacto")
+public class ContactoServlet extends HttpServlet {
+
+    private ContactoDAO dao;
+
+    @Override
+    public void init() {
+        dao = new ContactoDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String nombre = request.getParameter("nombre");
+        String email = request.getParameter("email");
+        String mensaje = request.getParameter("mensaje");
+
+        Contacto c = new Contacto(0, nombre, email, mensaje);
+
+        if (dao.guardarMensaje(c)) {
+            request.setAttribute("msg", "Mensaje enviado correctamente ✔");
+        } else {
+            request.setAttribute("msg", "Error al enviar mensaje ✘");
+        }
+
+        request.getRequestDispatcher("/WEB-INF/views/contacto.jsp")
+               .forward(request, response);
+    }
+}
